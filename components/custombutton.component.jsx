7947485b498cd34fbc04ec3c2b9f0c8a1b642a2d
saptarshibasu15 <123saptarshi.basu@gmail.com>
2021-05-07 16:50:10 +0530

    fixed lp-lvl-1

--- components/custombutton.component.jsx
+++ components/custombutton.component.jsx
@@ -1,5 +1,5 @@
 const CustomButton = ({title,classextend}) =>{
-  return (<button class={`outline-none duration-500 rounded-lg text-sm font-semibold px-6 py-2 ${classextend || ""}`}>{title}</button>)
+  return (<button class={`duration-500 rounded-lg text-sm font-semibold px-6 py-2 ${classextend || ""}`}>{title}</button>)
 }
 
 export default CustomButton;
