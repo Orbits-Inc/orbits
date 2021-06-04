042fa4d8b4ebccc833e9d49619f707fb8b998536
Saptarshi Basu <123saptarshi.basu@gmail.com>
2021-06-04 19:58:11 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -19,7 +19,7 @@ function MyApp({ Component, pageProps }) {
             />
 
             <meta property="og:type" content="website" />
-            <meta property="og:url" content="https://metatags.io/" />
+            <meta property="og:url" content="https://orbits-theta.vercel.app/" />
             <meta
               property="og:title"
               content="Orbits - Research | Learn | Share"
@@ -35,7 +35,7 @@ function MyApp({ Component, pageProps }) {
             />
 
             <meta property="twitter:card" content="summary_large_image" />
-            <meta property="twitter:url" content="https://metatags.io/" />
+            <meta property="twitter:url" content="https://orbits-theta.vercel.app/" />
             <meta
               property="twitter:title"
               content="Orbits - Research | Learn | Share"
