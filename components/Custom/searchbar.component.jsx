fdb7c9283e851f5393e07ca43256a1802c5529b9
Rishit Khandelwal <rishitkh@gmail.com>
2021-06-04 21:48:26 +0530

    Trim search queries

--- components/Custom/searchbar.component.jsx
+++ components/Custom/searchbar.component.jsx
@@ -11,11 +11,10 @@ function SearchBar() {
       className="w-full lg:w-80 hover:bg-gray-100 duration-500 bg-gray-200"
       required
       onChange={(e) => {
-        setQuery(e.target.value);
+        setQuery(e.target.value.trim());
       }}
       onSubmit={(e) => {
         e.preventDefault();
-
 
         if (query ) {
           window.location.href = `/search/${query}`;
