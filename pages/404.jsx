a22525298031775a82962438463c1925f57705d3
saptarshibasu15 <123saptarshi.basu@gmail.com>
2021-06-06 17:42:35 +0530

    fixed auth

--- pages/404.jsx
+++ pages/404.jsx
@@ -8,7 +8,6 @@ export default function NotFound() {
         <title>Orbits</title>
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <SignIn />
       <Page404 message="No such page exists, guess you got lost..." />
     </main>
   );
