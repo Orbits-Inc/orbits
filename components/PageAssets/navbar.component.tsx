61c3a14d8aecf5dbc9bad2375c7b58eaa3e2b5c6
AadityaMunjal <aadityamunjal06@gmail.com>
2021-06-21 09:12:18 +0530

    hide login button while signedin

--- components/PageAssets/navbar.component.tsx
+++ components/PageAssets/navbar.component.tsx
@@ -243,13 +243,15 @@ const Navbar = ({ className, loginText }: Navbar) => {
               </div>
             </div>
             <div>
-              <CustomButton
-                className="font-semibold border-2 border-blue-500 text-secondary w-full px-6 py-3 hover:bg-blue-500 hover:text-white rounded-full"
-                title="Login"
-                onClick={() => {
-                  window.location.href = "/auth";
-                }}
-              />
+              {!user && (
+                <CustomButton
+                  className="font-semibold border-2 border-blue-500 text-secondary w-full px-6 py-3 hover:bg-blue-500 hover:text-white rounded-full"
+                  title="Login"
+                  onClick={() => {
+                    window.location.href = "/auth";
+                  }}
+                />
+              )}
             </div>
           </div>
         </div>
