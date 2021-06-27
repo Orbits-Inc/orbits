8925da3912a90d7559e2bb87bfb40ddb3d5945c7
AadityaMunjal <aadityamunjal06@gmail.com>
2021-06-27 16:35:32 +0530

    update switcher

--- components/Misc/switcher.component.tsx
+++ components/Misc/switcher.component.tsx
@@ -7,33 +7,36 @@ interface SwitcherItem {
 }
 interface Switcher {
   items: SwitcherItem[];
+  switcherClassName?: string;
+  switcherItemClassName?: string;
+  switcherItemActiveClassName?: string;
 }
 
-const Switcher = ({ items }: Switcher): JSX.Element => {
+const Switcher = ({
+  items,
+  switcherClassName = "",
+  switcherItemClassName = "",
+  switcherItemActiveClassName = "",
+}: Switcher): JSX.Element => {
   const [activeItem, setActiveItem] = useState<number>(0);
-  return (
-    <div className="flex items-center space-x-4">
-      <div className="flex rounded-full border border-gray-250 rounded-full">
-        {items.map((Item, idx) => {
-          return (
-            <div
-              className={`cursor-pointer duration-300 rounded-full p-2 ${
-                activeItem === idx
-                  ? "bg-secondary text-white-default"
-                  : "bg-transparent text-secondary"
-              }`}
-              onClick={() => setActiveItem(idx)}
-              key={idx}
-            >
-              {Item.logo}
-            </div>
-          );
-        })}
-      </div>
 
-      <div className="text-secondary font-medium text-sm w-16">
-        {items[activeItem].title}
-      </div>
+  return (
+    <div className={`flex items-center space-x-4 ${switcherClassName}`}>
+      {items.map((item, idx) => {
+        return (
+          <div
+            className={`${switcherItemClassName} ${
+              activeItem === idx ? switcherItemActiveClassName : ""
+            } cursor-pointer`}
+            onClick={() => setActiveItem(idx)}
+          >
+            <div className="flex items-center">
+              <div className="pr-2">{item.logo}</div>
+              {item.title}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
