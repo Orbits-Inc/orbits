027e1abb5bf1ee6e0bb0645e06cbd3b6ab9df6a5
imsaptarshi <123saptarshi.basu@gmail.com>
2021-06-29 22:40:23 +0530

    added typings

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,20 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    "tailwindcss",
+    process.env.NODE_ENV === "production"
+      ? [
+          "@fullhuman/postcss-purgecss",
+          {
+            content: [
+              ".src/pages/**/*.{js,jsx,ts,tsx}",
+              ".src/components/**/*.{js,jsx,ts,tsx}",
+              ".src/sections/**/*.{js,jsx,ts,tsx}",
+            ],
+            defaultExtractor: (content) =>
+              content.match(/[\w-/:]+(?<!:)/g) || [],
+          },
+        ]
+      : undefined,
+    "postcss-preset-env",
+  ],
 };
