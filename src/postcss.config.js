b2c053dced6dd605e59c8f9905857f5bf74d211f
imsaptarshi <123saptarshi.basu@gmail.com>
2021-06-29 22:49:44 +0530

    added typings

--- src/postcss.config.js
+++ src/postcss.config.js
@@ -6,9 +6,9 @@ module.exports = {
           "@fullhuman/postcss-purgecss",
           {
             content: [
-              "./src/pages/**/*.{js,jsx,ts,tsx}",
-              "./src/components/**/*.{js,jsx,ts,tsx}",
-              "./src/sections/**/*.{js,jsx,ts,tsx}",
+              "./pages/**/*.{js,jsx,ts,tsx}",
+              "./components/**/*.{js,jsx,ts,tsx}",
+              "./sections/**/*.{js,jsx,ts,tsx}",
             ],
             defaultExtractor: (content) =>
               content.match(/[\w-/:]+(?<!:)/g) || [],
